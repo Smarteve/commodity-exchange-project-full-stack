58476ba95ca5d435c0bc372ebbe83ba03e0fae9d
Smarteve <evelynyyr11@gmail.com>
2023-10-23 10:13:01 -0700

    n changes

---
+++ project_phase1.py
@@ -0,0 +1,185 @@
+'''
+update2 
+
+1 put global variable of the main exchange_dict and commodity_dict into Main class, avoid using global variable 
+2 avoid having input in class, separate input from class and put down in actions  
+3 having clean class, avoid trivial things such as split and .join in a class, bundle it in private functions, separate public and private functions 
+4 decompose add_commodity funtion into add commodity and add price
+5 add type hints, add exceptions
+
+'''
+
+from typing import Dict,List 
+from datetime import datetime,date
+#exchange_dict: Dict[str,"Exchange"] = {} # key exchange name, value exchange object 
+#commodity_dict: Dict[str,"Commodity"] = {} # key commodity name, value commodity object
+
+
+
+class Main:
+    def __init__(self):
+        self.exchange_dict: Dict[str,"Exchange"] = {} # key exchange name, value exchange object 
+        self.commodity_dict: Dict[str,"Commodity"] = {} # key commodity name, value commodity object
+
+    def add_exchange(self,name: str,description:str,currency_sign:str) -> None:
+        if name in self.exchange_dict:
+            print("exchange already existed, add a new exchange")
+            return
+        else:
+            new_exchange = Exchange(name,description,currency_sign)
+            self.exchange_dict[name] = new_exchange
+
+    def add_commodity(self, commodity: "Commodity") -> None: 
+        #check if commodity exist
+        if commodity.name in self.commodity_dict:
+            raise ValueError(f"{commodity.name} already existed, pls enter a new commodity")
+        self.commodity_dict[commodity.name] = new_commodity
+         
+    def _missing_exchanges_list(self,exchange_list: list[str]) -> list[str]:
+        return [exchange for exchange in exchange_list if exchange not in self.exchange_dict]
+       
+    def view_exchange(self,name:str) -> str:
+        print(self.exchange_dict[name]) if name in self.exchange_dict else print(f"{name} does not exist")
+
+    def view_commodity(self,name:str) -> str: 
+        print(self.commodity_dict[name]) if name in self.commodity_dict else print('commodity not found')
+
+    def remove_exchange(self,name:str)-> None:
+        #delete from exchange main dic 
+        if name in self.exchange_dict:
+            del self.exchange_dict[name]
+            print(f"{name} has been removed from exchanges")
+        else:
+            print("exchange not found")
+            
+    def remove_commodity(self,name:str) -> None: 
+        # delete from the main commodity dict 
+        if name in self.commodity_dict:
+            del self.commodity_dict[name]
+            print(f"{name} has been removed from commodities")
+        else:
+            print("commodity not found")
+
+    def get_exchange(self, name: str) -> "Exchange":
+        return self.exchange_dict[name]
+
+
+def _parse_time(time) -> date:
+    time_object = datetime.strptime(time,"%Y-%m-%d")
+    return time_object.date()  # print only date
+
+
+class Exchange:
+    def __init__(self,name: str,description: str,currency_sign: str):
+        self.name = name
+        self.description = description
+        self.currency_sign = currency_sign
+        self.commodities = [] # a list of commodity names
+    def __str__(self):
+        if self.commodities: # when add exchange, only requires administrator to add name and intro first, if no commodities at first then dont display
+            return f"{self.name} is {self.description}."\
+            f"The list of the commodities currently trading in {self.name} is {','.join(self.commodities)}."
+        else:
+            return f"{self.name} is {self.description}."
+
+class Commodity:
+    def __init__(self,name: str,unit: str):
+        self.name = name 
+        self.unit = unit  
+        self.prices: list["Price"] = []
+
+    def __str__(self):
+        last_traded_price = max(self.prices, key = lambda price: price.time)
+        last_traded_exchange = last_traded_price.exchange
+        currency_sign = last_traded_exchange.currency_sign
+        info = []
+        info.append(f"{self.name} is traded at {','.join(self.exchanges)}.")
+        info.append(f"It is last traded at {currency_sign}{last_traded_price.value} in {last_traded_exchange.name} per {self.unit}.")
+        info.append(f"The full trading info is:")
+        for price in self.prices:
+            info.append(str(price))
+        return "\n".join(info)
+   
+class Price:
+    def __init__(self,exchange:Exchange,time:date,value:float):
+        self.exchange: Exchange = exchange
+        self.time = time
+        self.value = value
+       
+    def __str__(self):
+        currency_sign = self.exchange.currency_sign
+        return f"{currency_sign}{self.value} at {self.exchange.name} at {self.time}"
+    
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.add_exchange("TOMO","Japan's largest commodity futures exchanges","¥")
+    print("Welcome")
+    while True:
+        print("-----")
+        print("choose your action: ")
+        print("1 Add an exchange")
+        print("2 Add a commodity") 
+        print("3 View an exchange")
+        print("4 View an commodity")
+        print("5 Remove an exchange")
+        print("6 Remove a commodity")
+        print("7 Exit")
+        print("-----")
+        action = input("please choose a number (1 to 7): ")
+        if action == "1":
+            exchange_list = input("What's the name of the exchange you want to add? ")
+            description= input(f"What's the descripition of the exchange? {exchange_list} is: ")
+            currency_sign =  input(f"what's the primary currency of the exchange? choose from ¥,$,£ and yuan: ")
+            main.add_exchange(exchange_list,description,currency_sign)
+            print(f"Thanks for adding {exchange_list}")
+
+        if action == "2":
+            #add new commodity
+            commodity_name = input("What is the name of the commodity you want to add? ")
+            unit = input("What is the unit of the commodity? ")
+            exchange_list = input(f"what is the exchange {commodity_name} is traded at(comma separated if multiple): ").split(",")
+            prices = []
+
+            if missing_exchanges := main._missing_exchanges_list(exchange_list):
+                print(f"Missing exchanges: {missing_exchanges}")
+                continue
+
+            for exchange in exchange_list:
+                time = _parse_time(input(f"Enter the last traded time in {exchange}(yyyy-mm-dd): "))
+                price = float(input(f"Enter the price of last trade in {exchange}: "))
+                prices.append(Price(main.get_exchange(exchange), time, price))
+
+            try:
+                new_commodity = main.add_commodity(Commodity(commodity_name, unit, prices))
+            except ValueError as e:
+                print(e)
+                continue
+
+            #add price to the commodity 
+            for exchange in exchange_list:       
+                new_commodity.exchanges.append(exchange) # add exchange to commodity function **
+                time = input(f"Enter the last traded time in {exchange}(yyyy-mm-dd): ")
+                formatted_time = _parse_time(time)
+                price = float(input(f"Enter the price of last trade in {exchange}: "))
+            new_price = Price(main.exchange_dict[exchange],formatted_time,price)
+            new_commodity.prices.append(new_price) #update self.price dictionary under Commodity class
+            main.exchange_dict[exchange].commodities.append(commodity_name)# update self.commodities under Exchange class
+            main.commodity_dict[commodity_name] = new_commodity # update main commodity dic
+            print(f"Thanks for adding {commodity_name}")
+
+        if action == "3":
+            commodity_name = input("Which exchange do you want to view? ")
+            main.view_exchange(commodity_name)
+        if action == "4":
+            commodity_name = input("Which commodity do you want to view? ")
+            main.view_commodity(commodity_name)
+        if action == "5":
+            commodity_name = input("Enter the exchange you want to remove: ")
+            main.remove_exchange(commodity_name)
+        if action == "6":
+            commodity_name = input("Enter the commodity you want to remove: ")
+            main.remove_commodity(commodity_name)
+        if action == "7":
+            break 
--- test_phase1.py
+++ test_phase1.py
@@ -1,28 +1,33 @@
 import unittest 
-from unittest.mock import patch 
-from main_module import Exchange,Commodity,add_commodity
+import project_phase1
 
 class AddingCommodityTest(unittest.TestCase):
-    def setUp(self): 
-        global exchange_dict,commodity_dict
-        exchange_dict = {"TOMO": Exchange("TOMO", "Japan's largest exchange", "¥")}
-        commodity_dict = {}
-
-    def test_commodity_exists(self):
-        commodity_dict["gold"] = Commodity("gold","ounce")
-        self.assertTrue(_commodity_exist("gold"))
-        self.assertFalse(_commodity_exist("silver"))
     
-    def test_missing_exchange_list(self):
-        self.assertEqual(_missing_exchanges_list(["TOMO"]),[])
-        self.assertEqual(_missing_exchanges_list(["TOMO","NYSE"]),["NYSE"])
+    def test_add_exchange(self):
+        app = project_phase1.Main()
+        app.add_exchange("TOMO", "Japan's largest commodity futures exchanges", "¥")
+        self.assertIn("TOMO",app.exchange_dict)
+        self.assertIsInstance(app.exchange_dict["TOMO"],project_phase1.Exchange)
 
-    @patch('builtins.input',side_effect = ["2023-10-01", "1500"])
-    def test_add_commodity_to_system(self,mock_input):
-        _add_commodity_to_system("gold","ounce",["TOMO"])
-        self.assertIn("gold",commodity_dict)
-        self.assertIn("gold",exchange_dict["TOMO"].commodities)
+    def test_add_commodity(self):
+        app = project_phase1.Main()
+        app.add_exchange("TOMO", "Japan's largest commodity futures exchanges", "¥")
+        app.add_commodity("Gold", ["TOMO"], "kg")
+        self.assertIn("Gold", app.commodity_dict)
 
+    def test_remove_exchange(self):
+        app = project_phase1.Main()
+        app.add_exchange("TOMO", "Japan's largest commodity futures exchanges", "¥")
+        app.remove_exchange("TOMO")
+        self.assertNotIn("TOMO", app.exchange_dict)
+
+    def test_remove_commodity(self):
+        app = project_phase1.Main()
+        app.add_exchange("TOMO", "Japan's largest commodity futures exchanges", "¥")
+        app.add_commodity("Gold", ["TOMO"], "kg")
+        app.remove_commodity("Gold")
+        self.assertNotIn("Gold", app.commodity_dict)
+    
 
 if __name__ == "__main__":
     unittest.main()
