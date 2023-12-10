6db15a069e8080dadd0ff494bd0ebf5b8d30c778
Evelyn YU <evelynyyr11@gmail.com>
2023-12-10 23:28:41 +0000

    Implement db logic

--- db_logic.py
+++ db_logic.py
@@ -1,38 +1,107 @@
 import sqlite3
+from datetime import datetime, date
+from exceptions import DuplicateError, NotFoundError
 
-from tables import DB_FILE
-
-class DuplicateExchangeError(Exception):
-    pass
-
-
-# Exceptions
+DB_FILE = "exchange.db"
 
 
-def get_db_connection(db_path = DB_FILE):
+def get_db_connection(db_path=DB_FILE):
     return sqlite3.connect(db_path)
 
 
+CONN = get_db_connection()
+CONN.row_factory = sqlite3.Row  # return a dict like row object
+
+
 def add_exchange(name: str, description: str, currency_sign: str) -> None:
-    conn = get_db_connection()
-    with get_db_connection() as conn:
-        cursor = conn.cursor()
-        try:
-            cursor.execute("INSERT INTO Exchanges(name,description,currency_sign) VALUES(?,?,?)",(name,descripition,currency_sign))
-        except sqlite3.IntegrityError:
-            raise DuplicateExchangeError(f"Exchange with {name} already exists")
-   
+    cursor = CONN.cursor()
+    try:
+        cursor.execute(
+            "INSERT INTO Exchanges(name,description,currency_sign) VALUES(?,?,?)",
+            (name, description, currency_sign),
+        )
+    except sqlite3.IntegrityError:
+        raise DuplicateError(f"Record with {name} already exists")
+    CONN.commit()
 
 
-
-def view_exchange(name:str) -> str:
-    pass 
-
-def delete_exchange(name:str) -> None:
-    pass
-    
+def view_exchange(name: str) -> str:
+    cursor = CONN.cursor()
+    cursor.execute("SELECT name,description FROM Exchanges WHERE name=?", (name,))
+    result = cursor.fetchone()
+    return result
 
 
+def delete_exchange(name: str) -> None:
+    cursor = CONN.cursor()
+    cursor.execute("DELETE FROM Exchanges WHERE name=?", (name,))
+    if CONN.total_changes == 0:
+        raise NotFoundError(f"record not found in system")
+    CONN.commit()
 
 
+def _check_valid_exchanges(exchange_price_pairs: list[tuple]):
+    missing_exchanges = []
+    cursor = CONN.cursor()
+    for tup in exchange_price_pairs:
+        exchange = tup[0]
+        cursor.execute("SELECT * FROM Exchanges WHERE name=?", (exchange,))
+        result = cursor.fetchone()
+        if not result:
+            missing_exchanges.append(exchange)
+    if missing_exchanges:
+        raise NotFoundError(
+            f"following exchanges {','.join(missing_exchanges)} not found, pls add exchanges first"
+        )
 
+
+def _get_exchange_id(name: str) -> int:
+    cursor = CONN.cursor()
+    cursor.execute("SELECT id FROM Exchanges WHERE name=?", (name,))
+    result = cursor.fetchone()
+    return result["id"]
+
+
+def add_prices(security_id, exchange_price_pairs: list[tuple]) -> None:
+    cursor = CONN.cursor()
+    for exchange, price, time in exchange_price_pairs:
+        exchange_id = _get_exchange_id(exchange)
+        time_object = datetime.strptime(time, "%Y-%m-%d").date()
+        cursor.execute(
+            "INSERT INTO PRICES(security_id,exchange_id,price,time) VALUES(?,?,?,?)",
+            (security_id, exchange_id, price, time_object),
+        )
+    CONN.commit()
+
+
+def add_commodity(name: str, unit: str, exchange_price_pairs: list[tuple]) -> None:
+    _check_valid_exchanges(exchange_price_pairs)
+    cursor = CONN.cursor()
+    try:
+        cursor.execute(
+            "INSERT INTO Securities(name,type) VALUES(?,?)", (name, "commodity")
+        )
+    except sqlite3.IntegrityError:
+        raise DuplicateError(f"record with commodity {name} already exists")
+    security_id = cursor.lastrowid
+
+    cursor.execute("INSERT INTO Commodities(id,unit) VALUES(?,?)", (security_id, unit))
+
+    add_prices(security_id, exchange_price_pairs)
+    CONN.commit()
+
+
+def view_commodity(name: str) -> str:
+    cursor = CONN.cursor()
+    cursor.execute(
+        """
+        SELECT *, Exchanges.name as exchange_name FROM Securities
+            INNER JOIN Commodities ON Securities.id = Commodities.id
+            INNER JOIN Prices ON Securities.id = Prices.security_id
+            INNER JOIN Exchanges ON Prices.exchange_id = Exchanges.id
+            WHERE Securities.name = ?
+    """,
+        (name,),
+    )
+    security_id = cursor.fetchone()
+    return security_id
--- tables.py
+++ tables.py
@@ -1,51 +1,65 @@
 import sqlite3
+
 DB_FILE = "exchange.db"
-conn = sqlite3.connect(DB_FILE)
-cursor = conn.cursor()
 
 
-cursor.execute(
-    """CREATE TABLE Exchanges(
-               id INT PRIMARY KEY,
-               name VARCHAR(255) UNIQUE,
-               description TEXT,
-               currency_sign VARCHAR(10)
-)
+def main():
+    print("Creating tables")
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute(
+        """CREATE TABLE Exchanges(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(255) UNIQUE,
+                description TEXT,
+                currency_sign VARCHAR(10)
+    )
 
-"""
-)
+    """
+    )
 
-cursor.execute('''CREATE TABLE Securities(
-    id INT PRIMARY KEY,
-    name VARCHAR(225) UNIQUE,
-    type VARCHAR(10)
-)
-''')
+    cursor.execute(
+        """CREATE TABLE Securities(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(225) UNIQUE,
+        type VARCHAR(10)
+    )
+    """
+    )
+
+    cursor.execute(
+        """CREATE TABLE Commodities(
+        id INTEGER PRIMARY KEY,
+        unit TEXT, 
+        FOREIGN KEY (id) REFERENCES Securities(id)
+    )
+    """
+    )
+
+    cursor.execute(
+        """CREATE TABLE Currencies(
+        id INTEGER PRIMARY KEY,
+        type VARCHAR(50),
+        tenor VARCHAR(20), 
+        settlement_dates DATE,
+        FOREIGN KEY (id) REFERENCES Securities(id)
+    )
+    """
+    )
+
+    cursor.execute(
+        """CREATE TABLE Prices(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        security_id INT,
+        exchange_id INT,
+        price DECIMAL(10,2),
+        time DATE,
+        FOREIGN KEY (security_id) REFERENCES Securities(id),
+        FOREIGN KEY (exchange_id) REFERENCES Exchanges(id)
+    )
+    """
+    )
 
 
-cursor.execute('''CREATE TABLE Commodities(
-    id INT PRIMARY KEY,
-    unit TEXT, 
-    FOREIGN KEY (id) REFERENCES Securities(id)
-)
-''')
- 
-cursor.execute('''CREATE TABLE Currencies(
-    id INT PRIMARY KEY,
-    type VARCHAR(50),
-    tenor VARCHAR(20), 
-    settlement_dates DATE,
-    FOREIGN KEY (id) REFERENCES Securities(id)
-)
-''')
-
-cursor.execute('''CREATE TABLE Prices(
-    id INT PRIMARY KEY,
-    security_id INT,
-    exchange_id INT,
-    price DECIMAL(10,2),
-    time DATE,
-    FOREIGN KEY (security_id) REFERENCES Securities(id),
-    FOREIGN KEY (exchange_id) REFERENCES Exchanges(id)
-)
-''')
+if __name__ == "__main__":
+    main()
